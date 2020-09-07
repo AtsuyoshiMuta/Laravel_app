86672f33bc56f663b00dfe0f3a40448e7b980aec
Atsuyoshi Muta <atsuyoshimuta@adminnoMacBook-Pro-8.local>
2020-09-07 19:08:50 +0900

    final commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,19 +1,18 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <style>
-    form{
+    .box{
         margin-top: 50px;
     }
 
     input{
         border-width: 1px;
-        border-color: #ccc;
+        border-color: #cccccc;
         border:none;
         /*background:none;*/
     }
 
     .btn{
         color: #5a6268;!important;
-        margin: 40px 0px 0px 350px;
         border: solid 1px;
         border-radius: 5px;
     }
@@ -33,19 +32,18 @@
 </style>
 
 <div class="container">
-    <div class="row">
-        <form class="col-6" action="/main" method="get">
-            <input class="rounded-pill" type="text" name="query">
-            <input class="rounded-pill" type="submit" value="search">
-        </form>
-        <div class="col">
-            <div class="btn">
+    <div class="row justify-content-center">
+        <div class="col-10 row box">
+            <form class="col-4" action="/main" method="get">
+                <input class="rounded-pill" type="text" name="query">
+                <input class="rounded-pill" type="submit" value="search">
+            </form>
+            <div class="col"></div>
+            <div class="col-3 btn">
                 <a href="{{route('post')}}">New post.</a>
             </div>
         </div>
-    </div>
-
-    <div class="row justify-content-center">
+        <div class="w-100"> </div>
         @foreach($items as $item)
             <div class="col-5 card">
                 <a href="{{route('vote',['id' => $item->id])}}"><strong>{{$item->title}}</strong></a>
